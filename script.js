62d152a426c7ee521334b251f83052ffd413ce20
anikethgalla <anikethgalla@gmail.com>
2026-03-04 09:00:49 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let h=document.querySelector(".hourhand")
+let m=document.querySelector(".minutehand")
+let s=document.querySelector(".secondhand")
+
+function Time(){
+    let date = new Date();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
+
+    if(hour<10){
+        hour = "0" + hour;
+    }
+    if(minute<10){
+        minute = "0" + minute;
+    }
+    if(second<10){
+        second = "0" + second;
+    }
+    h.textContent=hour;
+    m.textContent=minute;
+    s.textContent=second;
+}
+setInterval(Time,1000);
+Time();
