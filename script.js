a55c9982d157491f15dcc4333024702656ca475e
anikethgalla <anikethgalla@gmail.com>
2026-03-05 12:33:42 +0530

    Intial commit

--- script.js
+++ script.js
@@ -23,3 +23,24 @@ function Time(){
 }
 setInterval(Time,1000);
 Time();
+
+function date(){
+    let date = new Date();
+    let year=date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    if(month<10){
+        month = "0" + month;
+    }
+    if(day<10){
+        day = "0" + day;
+    }
+
+    document.getElementById("date").textContent ="Date: " + day + "/" + month + "/" + year;
+
+    const dayoftheweek = date.getDay();
+    const dayNames = ["Sunday", "Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    document.getElementById("day").textContent ="Day: " + dayNames[dayoftheweek];
+}
+
+date();
